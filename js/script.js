1b94b7525088a001e8914a79a074909ac1418719
root <root@DESKTOP-V1UMGO9.localdomain>
2021-06-24 23:17:50 -0600

    adding CSS style & dice roll functionality

---
+++ js/script.js
@@ -0,0 +1,36 @@
+// The code below appends to an element
+
+// let node = document.createElement('span');
+// node.style.height = "50px";
+// node.style.width = "25px";
+// node.style.display = "inline-block";
+// node.style.backgroundColor = "#FFA500";
+// node.textContent = '';
+// let startEl = document.querySelector("#start");
+// console.log(node);
+// console.log(startEl);
+// startEl.append(node);
+
+// let node2 = document.createElement('span');
+// node2.style.height = "50px";
+// node2.style.width = "25px";
+// node2.style.display = "inline-block";
+// node2.style.backgroundColor = "#FFA500";
+// node2.textContent = '';
+// // let startEl = document.querySelector("#start");
+// startEl.append(node2);
+
+// Pseudo code
+
+// Dice Roll function
+function diceRoll() {
+    return Math.floor(Math.random() * (7 - 1) + 1);
+}
+
+// Player 1 and Player 2 points counter
+let p1Counter = 0;
+let p2Counter = 0;
+
+// MOve function
+    // append step by step
+    // checks if the player can choose their direction.
