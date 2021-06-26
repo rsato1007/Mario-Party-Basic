c6ec5732307e5cbec86a84a7dbbb057e0a813f8c
root <root@DESKTOP-V1UMGO9.localdomain>
2021-06-25 20:50:31 -0600

    adding player ability to move around board

--- js/script.js
+++ js/script.js
@@ -6,10 +6,7 @@
 // node.style.display = "inline-block";
 // node.style.backgroundColor = "#FFA500";
 // node.textContent = '';
-// let startEl = document.querySelector("#start");
-// console.log(node);
-// console.log(startEl);
-// startEl.append(node);
+let startEl = document.querySelector("#start");
 
 // let node2 = document.createElement('span');
 // node2.style.height = "50px";
@@ -21,16 +18,90 @@
 // startEl.append(node2);
 
 // Pseudo code
+let diceNum;
+let movesNum;
 
-// Dice Roll function
-function diceRoll() {
-    return Math.floor(Math.random() * (7 - 1) + 1);
+// Player objects
+const player1 = {
+    points: 0,
+    position: 0,
 }
 
-// Player 1 and Player 2 points counter
-let p1Counter = 0;
-let p2Counter = 0;
+const player2 = {
+    path: 0,
+    points: 0,
+    position: 0,
+    charModel: document.createElement('span'),
+    currentSpaceEl: "",
+    // creates character on board
+    char() {
+        this.charModel.style.height = "40px";
+        this.charModel.style.width = "20px";
+        this.charModel.style.display = "inline-block";
+        this.charModel.style.backgroundColor = "#FFA500";
+        this.charModel.textContent = '';
+    },
+    playerTurn() {
 
-// MOve function
-    // append step by step
-    // checks if the player can choose their direction.
+    },
+    movePlayer() {
+        for (i = 0; i < diceNum; i++) {
+            if (player2.position === 0) {
+                player2.position += 1;
+                console.log(`You moved 1 space and are at position: ${this.position}`);
+            }
+            else if (player2.position === 49) {
+                player2.position -= 1;
+                console.log(`You moved 1 space and are at position: ${this.position}`);
+            }
+            else if (player2.position === 43) {
+                player2.position -= 7;
+                console.log(`You moved 1 space and are at position: ${this.position}`);
+            }
+            else if (player2.position === 44 || player2.position === 45 || player2.position === 46 || player2.position === 47 || player2.position === 48) {
+                player2.position -= 1;
+                console.log(`You moved 1 space and are at position: ${this.position}`);
+            }
+            else if (player2.position % 7 === 0) {
+                player2.position += 7;
+                console.log(`You moved 1 space and are at position: ${this.position}`);
+            } 
+            else if ((player2.position % 7 === 1) && (player2.position === 1)) {
+                player2.position += 1;
+                console.log(`You moved 1 space and are at position: ${this.position}`);
+            }
+            else if (player2.position % 7 === 1) {
+                player2.position -= 7;
+                console.log(`You moved 1 space and are at position: ${this.position}`);
+            } 
+            else {
+                player2.position += 1;
+                console.log(`You moved 1 space and are at position: ${this.position}`);
+            }
+        }
+        player2.char();
+        currentSpaceEl = document.getElementById(`square${this.position}`);
+        currentSpaceEl.append(this.charModel);
+        this.updatePoints();
+    },
+    updatePoints() {
+        if (currentSpaceEl.classList.contains('blue-space')) {
+            player2.points += 3;
+        }
+    }
+}
+
+// Dice Roll function
+function rollDice() {
+    diceNum = Math.floor(Math.random() * (7 - 1) + 1);
+    console.log(`You rolled a ${diceNum}`);
+}
+
+// testing ground
+rollDice();
+player2.movePlayer();
+
+for (let i = 0; i < 6; i++) {
+    rollDice();
+    player2.movePlayer(); 
+}
